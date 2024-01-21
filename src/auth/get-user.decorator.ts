4e804e406ce9705c2338654e5030c5a4c1103915
veeru78866 <veeru78866@gmail.com>
2024-01-21 15:43:49 +0530

    Auth Guard updated

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -1,8 +1,7 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { User } from "./user.entity";
-import { UserCredintialsDto } from "./dtos/user-credintials.dto";
 
-export const GetUser = createParamDecorator((_data, ctx:ExecutionContext)=>{
+export const GetUser = createParamDecorator((_data, ctx:ExecutionContext): User=>{
     const req = ctx.switchToHttp().getRequest();
     return req?.user;
 });
